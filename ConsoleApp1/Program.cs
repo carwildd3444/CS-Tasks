cc2d43fc803abe08eb0941a5e53895f75a4300b2
mka <mka@A34PC14>
2024-09-16 14:12:42 +0300

    cncncncnvcn

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Skiki gradusv - ");
+//Console.Write("Skiki gradusv - ");
+//decimal C = decimal.Parse(Console.ReadLine());
+
+//Console.Write("Vot skiki F - ");
+//Console.Write(C * 1.8m + 32);
+
+Console.Write("Obertu - ");
 decimal C = decimal.Parse(Console.ReadLine());
-
-Console.Write("Vot skiki F - ");
-Console.Write(C * 1.8m + 32);
-
-
+decimal D = 686;
+Console.Write("Vot skiki day - ");
+Console.Write(C * D);
 
 
